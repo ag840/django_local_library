ab0ed2c8beb04a901902564a504265f4efa3f421
Adrian <ag835@njit.edu>
2025-05-06 18:16:57 -0400

    Track number of user homepage visits via sessions

--- catalog/views.py
+++ catalog/views.py
@@ -22,6 +22,10 @@ def index(request):
     # The 'all()' is implied by default
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -29,6 +33,7 @@ def index(request):
         'num_authors': num_authors,
         'num_witcher_books': num_witcher_books,
         'num_fiction': num_fiction,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
